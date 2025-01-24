cca7a9eca4c7683aa9ae49870146fee803384828
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-01-24 15:31:59 -0500

    started UI

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Navbar() {
 return (
-    <div>
-        <h1>Atomic Moss</h1>
-    </div>
+    <header className="mb-8 border-b">
+        <div className="flex items-center justify-between mx-auto max-w-2xl sm:px6 lg:max-w-7xl">
+            <Link href="/">
+                <h1 className="text-4xl font-bold">
+                    Next<span className="text-primary">Commerce</span>
+                </h1>
+            </Link>
+        </div>
+    </header>
     );
 }
