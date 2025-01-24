3274f1fbbee3f779653dabecd8f2387e551a23fe
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-01-24 17:17:20 -0500

    Added Hero file and modified other files

---
+++ app/components/Hero.tsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+    return (
+        <section className="mx-auto max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:px-8">
+            <div className="mb-8 flex flex-wrap justify-between md:mb-16">
+                <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
+                    <h1>
+                        Hello
+                    </h1>
+                </div>
+            </div>
+        </section>
+    )
+}
