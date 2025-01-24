c319828931a94c66b6b6c6799912514a5b84aa2d
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-01-23 22:31:14 -0500

    added files

---
+++ sanity/schemas/product.ts
@@ -0,0 +1,34 @@
+export default {
+    name: 'product',
+    type: 'document',
+    title: 'Product',
+    fields: [
+        {
+            name: 'name',
+            type: 'string',
+            title: 'Name of Product',
+        },
+        {
+            name: 'images',
+            type: 'array',
+            title: 'Product Images',
+            of: [{type: 'image'}],
+        },
+        {
+            name: 'description',
+            type: 'text',
+            title: 'Description of Product',
+        },
+        {
+            name: 'slug',
+            type: 'slug',
+            title: 'Product Slug',
+        },
+        {
+            name: 'price',
+            title: 'Price',
+            type: 'number',
+        },
+    ],
+}
+
