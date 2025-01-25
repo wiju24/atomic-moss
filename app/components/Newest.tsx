88437ecaa2a44eef4dbc57d9f08b95d97e4f5698
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-01-25 18:03:20 -0500

    fixed bug from newest file

--- app/components/Newest.tsx
+++ app/components/Newest.tsx
@@ -1,15 +1,16 @@
-import { ArrowRight, Link } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 import { simplifiedProduct } from "../interface";
 import { client } from "../lib/sanity";
 import Image from "next/image"
+import Link from "next/link";
 
 async function getData() {
-    const query = `*[_type == "product"[0...4] | order(_createdAt desc){
-        _id
+    const query = `*[_type == "product"][0...4] | order(_createdAt desc){
+        _id,
             price,
         name,
             "slug": slug.current,
-            "categoryName": category->name
+            "categoryName": category->name,
             "imageUrl": images[0].asset->url
     }`; 
 
