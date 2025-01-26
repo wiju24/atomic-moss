cf834af992850011788ac87ba0159a9876d2815b
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-01-26 02:28:49 -0500

    Started working on individual product page

--- app/components/Newest.tsx
+++ app/components/Newest.tsx
@@ -41,8 +41,24 @@ export default async function Newest() {
                                     alt="Product Image" 
                                     className="w-full h-full object-cover object-center lg:h-full lg:w-full" 
                                     width={300} 
-                                    height={300} /> 
+                                    height={300} 
+                                /> 
                             </div>
+                            <div className="mt-4 flex justify-between">
+                                <div>
+                                    <h3 className="text-sm text-gray-700">
+                                    <Link href={`/product/s{product.slug}`}>
+                                        {product.name}
+                                    </Link>
+                                    </h3>
+                                    <p className="mt-1 text-sm text-gray-500">
+                                        {product.categoryName}
+                                    </p>
+                                </div>
+                            </div>
+                            <p className="text-sm font-medium text-gray-900">
+                               ${product.price} 
+                            </p>
                         </div>
                     ))}
                 </div>
