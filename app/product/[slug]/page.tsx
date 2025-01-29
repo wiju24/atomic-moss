67b16887d0fea10a5049376a39c82a6c80ed6a31
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-01-29 17:36:16 -0500

    modified files

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -26,7 +26,7 @@ export default async function ProductPage({
     const data: fullProduct = await getData(params.slug);
 
         return (
-        <div className="bg-white" >
+        <div className="bg-white">
             <div className="mx-auto max-w-screen-xl px-4 md:px-8">
                 <div className="grid gap-8 md:grid-cols-2">
                     <ImageGallery images={data.images}/>
