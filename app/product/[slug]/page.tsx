72fcc4903b956d3613168b16c0de319bf1a6d96c
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-01-30 21:41:10 -0500

    --

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -10,29 +10,50 @@ async function getData (slug: string) {
             name,
             description,
             "slug": slug.current,
-            "categoryName": category->name,
+            "categoryName": category->name
         }`;
+
+
     const data = await client.fetch(query);
 
     return data;
 };
 
-export default async function ProductPage({
+export default async function ProductPge({
     params,
 }: {
-    params: {slug:string};
+    params: {slug: string};
+
 }) {
-
-    const data: fullProduct = await getData(params.slug);
-
-        return (
+    const { slug } = await params;
+    const data: fullProduct = await getData(slug)
+    return (
         <div className="bg-white">
             <div className="mx-auto max-w-screen-xl px-4 md:px-8">
                 <div className="grid gap-8 md:grid-cols-2">
-                    <ImageGallery images={data.images}/>
+                    <ImageGallery images={data?.images || []} />
+
                 </div>
             </div>
         </div>
     )
+
 }
+//     params
+// }: {
+//     params: {slug:string};
+// }) {
+
+//     const data: fullProduct = await getData(params.slug);
+
+//     return (
+//     <div className="bg-white">
+//         <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+//             <div className="grid gap-8 md:grid-cols-2">
+//                 <ImageGallery images= {data.images}/>
+//             </div>
+//         </div>
+//     </div>
+//     )
+// }
 
