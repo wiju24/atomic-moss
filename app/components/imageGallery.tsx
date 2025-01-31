5a398c95fcdb906073dca8368827a2bac9d172d3
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-01-31 18:33:59 -0500

    Created the individual product page and added the UI

--- app/components/imageGallery.tsx
+++ app/components/imageGallery.tsx
@@ -10,7 +10,11 @@ interface iAppProps {
 }
 
 export default function ImageGallery({images}: iAppProps) {
-  const [bigImage, setImage] = useState(images[0]);
+  const [bigImage, setBigImage] = useState(images[0]);
+
+  const handleSmallImageClick = (image: any) => {
+    setBigImage(image); 
+  }
   return (
     <div className="grid gap-4 lg:grid-cols-5">
       <div className="order-last flex gap-4 lg:order-none lg:flex-col">
@@ -21,7 +25,9 @@ export default function ImageGallery({images}: iAppProps) {
               width={200} 
               height={200} 
               alt="photo" 
-              className="h-full w-full object-cover object-center cursor-pointer"/>
+              className="h-full w-full object-cover object-center cursor-pointer"
+              onClick={() => handleSmallImageClick(image)}
+              />
           </div>
         ))}
       </div>
@@ -32,9 +38,11 @@ export default function ImageGallery({images}: iAppProps) {
           alt="Photo"
           width={500}
           height={500}
-          className="h-full w-fulll object-cover object-center"
+          className="h-full w-full object-cover object-center"
         />
-
+        <span className="absolute left-0 top-0 rounded-br-lg bg-red-500 px-3 py-1.5 text-sm uppercase tracking-wider text-white">
+          Sale
+        </span>
       </div>
     </div>
   )
