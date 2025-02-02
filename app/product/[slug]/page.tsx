691a86fec71d3ad3373c9a0a348e6d7cec6746ca
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-02-02 11:42:58 -0500

    Fixed Checkout now button to individually Checkout products

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import ImageGallery from "@/app/components/ImageGallery";
 import { Button } from "@/components/ui/button";
 import { Star, Truck } from "lucide-react";
 import AddToBag from "@/app/components/AddToBag";
+import CheckoutNow from "@/app/components/CheckoutNow";
 
 async function getData (slug: string) {
     const query = `*[_type == "product" && slug.current == "${slug}"][0] {
@@ -81,7 +82,15 @@ export default async function ProductPge({
                             key={data._id}
                             price_id={data.price_id}
                             />
-                            <Button variant={"secondary"}>Checkout</Button>
+                            <CheckoutNow 
+                                currency="CAD" 
+                                description={data.description} 
+                                image={data.images[0]} 
+                                name={data.name} 
+                                price={data.price} 
+                                key={data._id}
+                                price_id={data.price_id}
+                            />
                         </div>
                         <p className="mt-12 text-base text-gray-500 tracking-wide">
                             {data.description}
