f5aa3ce6fa465b9a30cc712ca7e7da590e16d761
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-02-01 20:16:38 -0500

    added more buttons to home page

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -53,8 +53,18 @@ export default async function Hero() {
                 <div className="flex h-12 w-64 divide-x overflow-hidden rounded-lg border">
                     <Link 
                         href="/Product" 
-                        className="flex w-1/3 items-center jsutify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">
+                        className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">
                         Shop All
+                    </Link>
+                    <Link 
+                        href="/Product" 
+                        className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">
+                        About
+                    </Link>
+                    <Link 
+                        href="/Product" 
+                        className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">
+                        Contact
                     </Link>
                 </div>
             </div>
