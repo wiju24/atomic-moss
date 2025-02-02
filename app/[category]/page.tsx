ea0a4f13ecf2086c3b51a78c27c0da97f8799a72
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-02-02 02:31:14 -0500

    updated files and readjusted config

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -21,19 +21,21 @@ async function getData(category: string) {
 export const dynamic = "force-dynamic";
 
 export default async function CategoryPage({params}: {params: {category: string}}) {
-    const data: simplifiedProduct[] = await getData(params.category);
+    const {category} = await params;
+
+    const data: simplifiedProduct[] = await getData(category);
         return (
             <div className="bg-white">
             <div className=" mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
                 <div className="flex justify-between items-center">
-                    <h2 >Products for {params.category}</h2>
+                    <h2 >Products for {category}</h2>
                 </div>
                 <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                    {data.map((product)=>(
-                        <div key={product._id} className="group relative">
+                    {data.map((category)=>(
+                        <div key={category._id} className="group relative">
                             <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
                                 <Image                                     
-                                    src={product.imageUrl} 
+                                    src={category.imageUrl} 
                                     alt="Product Image" 
                                     className="w-full h-full object-cover object-center lg:h-full lg:w-full" 
                                     width={300} 
@@ -43,16 +45,16 @@ export default async function CategoryPage({params}: {params: {category: string}
                             <div className="mt-4 flex justify-between">
                                 <div>
                                     <h3 className="text-sm text-gray-700">
-                                    <Link href={`/product/${product.slug}`}>
-                                        {product.name}
+                                    <Link href={`/product/${category.slug}`}>
+                                        {category.name}
                                     </Link>
                                     </h3>
                                     <p className="mt-1 text-sm text-gray-500">
-                                        {product.categoryName}
+                                        {category.categoryName}
                                     </p>
                                 </div>
                                 <p className="text-sm font-medium text-gray-900">
-                                    ${product.price} 
+                                    ${category.price} 
                                 </p>
                             </div>
 
