bcc2c86661c17376fbb13f71f89ea6a8a1d3bf16
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-02-02 01:49:20 -0500

    updated shadcn package

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import {Inter} from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar'
 import CartProvider from './components/Providers'
-import ShoppingCartModal from './components/ShoppingCartModal'
+// import ShoppingCartModal from './components/ShoppingCartModal'
 
 const inter = Inter ({ subsets: ['latin']})
 
@@ -22,7 +22,7 @@ export default function RootLayout ({
       <body className={inter.className}>
         <CartProvider>
           <Navbar/>
-          <ShoppingCartModal />
+          {/* <ShoppingCartModal /> */}
           {children}
         </CartProvider>
       </body>
