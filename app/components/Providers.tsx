a601091446f340c93f00af0623f2a46cf253f5a3
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-02-02 01:20:57 -0500

    modified files

---
+++ app/components/Providers.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { CartProvider as USCProvider } from "use-shopping-cart";
+import { ReactNode } from "react";
+
+export default function CartProvider({children}: {children:ReactNode}) {
+    return (
+        <USCProvider 
+            mode="payment" 
+            cartMode="client-only" 
+            stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
+            successUrl="http://localhost.3000/success"
+            cancelUrl="http://localhost.3000/error"
+            currency="CAD"
+            billingAddressCollection={true}
+            shouldPersist={true}
+            language="en-US"
+        >
+            {children}
+        </USCProvider>
+    )
+}
