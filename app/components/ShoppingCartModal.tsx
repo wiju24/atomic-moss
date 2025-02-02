d2c834c1a1305a073740374c72da020c647c47a5
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-02-02 01:49:03 -0500

    add shadcn package

---
+++ app/components/ShoppingCartModal.tsx
@@ -0,0 +1,21 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetHeader,
+    SheetTitle,
+  } from "@/components/ui/sheet"
+
+
+export default function ShoppingCartModal() {
+    <Sheet defaultOpen>
+
+    <SheetContent>
+      <SheetHeader>
+        <SheetTitle>Edit profile</SheetTitle>
+
+      </SheetHeader>
+
+
+    </SheetContent>
+  </Sheet>
+}
