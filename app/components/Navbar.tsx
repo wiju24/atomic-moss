8256ce418c6d6332650568186c1c37c13c414860
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-02-02 03:19:40 -0500

    Updated Cart functionality

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,15 +4,17 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import {ShoppingBag} from 'lucide-react'
+import { useShoppingCart } from "use-shopping-cart";
 
 const links = [
     {name: "Home", href: "/"},
     {name: "Bottles", href: "/Bottles"},
-    {name: "Topicals", href: "/Topicals"},
+    {name: "Topicals", href: "/Topical"},
 ]
 
 export default function Navbar() {
-    const pathname = usePathname()
+    const pathname = usePathname();
+    const {handleCartClick} = useShoppingCart();
 return (
     <header className="mb-8 border-b">
         <div className="flex items-center justify-between mx-auto max-w-2xl sm:px6 lg:max-w-7xl">
@@ -39,7 +41,11 @@ return (
             </nav>
 
             <div className="flex divide-x border-r sm:border-l">
-                <Button variant={"outline"}  className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none">
+                <Button 
+                    variant={"outline"}
+                    onClick={() => handleCartClick()}  
+                    className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none"
+                    >
                     <ShoppingBag />
                     <span className="hidden text-xs font-semibold text-gray-500 sm:block">Cart</span>
                 </Button>
