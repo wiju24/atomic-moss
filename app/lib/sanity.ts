62a7dba3ba29c4b3b5617838fec82bcc9c81c1d8
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-08-01 19:06:19 -0400

    Fix ESLint error in sanity.ts by disabling rule for Sanity image source

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -10,6 +10,7 @@ export const client = createClient({
 
 const builder = imageUrlBuilder(client)
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function urlFor(source: any) {
     return builder.image(source);
 }
