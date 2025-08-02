27c521ad33a9f0546f7299f8c30afdacaef50d17
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-08-01 20:20:08 -0400

    Fix ESLint error by using unknown type with type assertion

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -10,7 +10,6 @@ export const client = createClient({
 
 const builder = imageUrlBuilder(client)
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function urlFor(source: any) {
-    return builder.image(source);
+export function urlFor(source: unknown) {
+    return builder.image(source as any);
 }
