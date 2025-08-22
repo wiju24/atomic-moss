216af451c579b6a2b351584a87c0ace00531bbcd
Vij <vijithiran_navajeevayokan@hotmail.com>
2025-08-22 18:01:57 -0400

    Remove 'Products for {category}' heading from category page

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -27,9 +27,6 @@ export default async function CategoryPage({params}: {params: Promise<{category:
         return (
             <div className="bg-white">
             <div className=" mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-                <div className="flex justify-between items-center">
-                    <h2 >Products for {category}</h2>
-                </div>
                 <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                     {data.map((product)=>(
                         <div key={product._id} className="group relative">
